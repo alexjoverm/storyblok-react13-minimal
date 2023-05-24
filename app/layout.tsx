f0a446f5cbd3279329fae664483d3b64831f3149
Alex Jover <alexjovermorales@gmail.com>
2023-05-24 13:17:31 +0200

    add ts support

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+
+storyblokInit({
+  accessToken: "TIgDbA6g3L7Q5U05bj4Trwtt",
+  use: [apiPlugin],
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <StoryblokProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </StoryblokProvider>
+  );
+}
